404b5bc8d46f3b4b5b255a993f3266b837f44f9c
James <Icedshot@gmail.com>
2018-12-11 15:32:01 +0000

    beginning of card manager stuff

---
+++ Fartifact/Source/Fartifact/CardManager.cpp
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CardManager.h"
+#include <assert.h>
+
+std::vector<std::string> Card::short_names
+{
+	"$",
+	"£",
+	"₫",
+	"₽",
+	"€",
+	"₱",
+	"₹",
+	"B",
+	"🥞",
+	"🦘",
+	"Z$",
+	"¥"
+	"ERR",
+	"ERR"
+};
+
+std::string Card::get_short_name()
+{
+	int itype = (int)which;
+
+	if (itype < 0 || itype >= (int)type::COUNT)
+		throw std::runtime_error("Bad type in card get_short_name");
+
+	return short_names[itype];
+}
+
+CardManager::CardManager()
+{
+	assert(Card::short_names.size() == (int)Card::type::NONE);
+}
+
+CardManager::~CardManager()
+{
+}
---
+++ Fartifact/Source/Fartifact/CardManager.h
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include <vector>
+#include <string>
+
+struct Card
+{
+	static std::vector<std::string> short_names;
+
+	enum class type
+	{
+		AMERICAN_DOLLAR = 0,
+		POUND = 1,
+		DONG = 2,
+		RUBLE = 3,
+		EURO = 4,
+		PESO = 5,
+		RUPEE = 6,
+		BUTTONS = 7,
+		CANADIAN_DOLLAR = 8,
+		AUSTRALIAN_DOLLARYDOO = 9,
+		ZIMBABWEAN_DOLLARS = 10,
+		YUAN = 11,
+		COUNT,
+		NONE
+	};
+
+	std::string get_short_name();
+
+	type which = type::NONE;
+};
+
+/**
+ * 
+ */
+class FARTIFACT_API CardManager
+{
+public:
+	CardManager();
+	~CardManager();
+};
